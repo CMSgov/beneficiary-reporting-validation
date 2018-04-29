0a97ce9c4d071c3d38256cfb86fb378dc504ef93
JR McEntee <john.mcentee@semanticbits.com>
2018-04-29 07:41:53 -0400

    Seperate configs for prod and dev

---
+++ webpack.prod.ts
@@ -0,0 +1,16 @@
+import merge from 'webpack-merge';
+import common from './webpack.common';
+import * as webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const config: webpack.Configuration = merge(common, {
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static'
+    })
+  ]
+});
+
+export default config;
