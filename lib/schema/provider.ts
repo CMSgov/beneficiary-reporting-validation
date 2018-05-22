5fba86f936e1ca3e5fa2d5ec4070db306018faaa
PetePlays <peteplays@hotmail.com>
2018-05-22 11:04:27 -0400

    updated provider schema

--- lib/schema/provider.ts
+++ lib/schema/provider.ts
@@ -2,9 +2,6 @@ import * as Joi from 'joi';
 import { Regexes } from '../regexes';
 
 export const ProviderSchema = Joi.object().keys({
-  id: Joi.any(),
-  created_at: Joi.any(),
-  updated_at: Joi.any(),
   npi: Joi.string().max(10).regex(Regexes.numbersOnly).required(),
   firstName: Joi.string().max(50).regex(Regexes.lettersAndSymbolsOnly).required(),
   lastName: Joi.string().max(50).regex(Regexes.lettersAndSymbolsOnly).required(),
