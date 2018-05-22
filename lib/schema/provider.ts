c72fd46b46c0bbac17ef9bf8e34bd314b8257ce9
PetePlays <peteplays@hotmail.com>
2018-05-22 12:12:43 -0400

    added orgIdt to provider schema

--- lib/schema/provider.ts
+++ lib/schema/provider.ts
@@ -6,5 +6,6 @@ export const ProviderSchema = Joi.object().keys({
   firstName: Joi.string().max(32).regex(Regexes.lettersAndSymbolsOnly).required(),
   lastName: Joi.string().max(32).regex(Regexes.lettersAndSymbolsOnly).required(),
   ein: Joi.string().max(32).regex(Regexes.lettersAndNumbersOnly).optional().allow(null),
-  credentials: Joi.string().max(128).regex(Regexes.validCredentials).optional().allow(null)
+  credentials: Joi.string().max(128).regex(Regexes.validCredentials).optional().allow(null),
+  organizationId: Joi.number().optional().allow(null)
 });
