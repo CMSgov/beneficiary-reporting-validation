acac413775a1c2973227699ff39792d25f74ee90
PetePlays <peteplays@hotmail.com>
2018-05-22 14:39:19 -0400

    added `optional` to phone

--- lib/schema/organizationContact.ts
+++ lib/schema/organizationContact.ts
@@ -5,6 +5,6 @@ export const OrganizationContactSchema = Joi.object().keys({
   firstName: Joi.string().max(32).regex(Regexes.lettersAndSymbolsOnly).required(),
   lastName: Joi.string().max(32).regex(Regexes.lettersAndSymbolsOnly).required(),
   email: Joi.string().max(100).regex(Regexes.email).required(),
-  phone: Joi.string().length(10).regex(Regexes.numbersOnly).allow(null).empty(''),
+  phone: Joi.string().length(10).regex(Regexes.numbersOnly).optional().allow(null).empty(''),
   phoneExtension: Joi.string().optional().max(6).allow(null).empty('')
 });
