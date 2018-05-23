6a9d98d340ea0a93d43f72d82b9b40221323ab28
PetePlays <peteplays@hotmail.com>
2018-05-23 15:58:48 -0400

    added enum and max lengths

--- lib/schema/notification.ts
+++ lib/schema/notification.ts
@@ -1,10 +1,15 @@
 import * as Joi from 'joi';
 import { Regexes } from '../regexes';
 
+export enum Notification {
+  Error = 'ERROR',
+  Notification = 'NOTIFICATION'
+}
+
 export const NotificationSchema = Joi.object().keys({
-  userId: Joi.number().required(),
-  organizationId: Joi.number().required(),
-  messageType: Joi.string().required(),
-  message: Joi.string().required(),
+  userId: Joi.number().max(11).required(),
+  organizationId: Joi.number().max(11).required(),
+  messageType: Joi.string().valid(Notification.Error, Notification.Notification).required(),
+  message: Joi.string().max(255).required(),
   read: Joi.boolean().required()
 });
