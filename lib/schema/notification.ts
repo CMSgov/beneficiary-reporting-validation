06eb35ca826d39ec61808c100c51fea9553fdecd
PetePlays <peteplays@hotmail.com>
2018-05-23 15:48:21 -0400

    added read attr as required

--- lib/schema/notification.ts
+++ lib/schema/notification.ts
@@ -5,5 +5,6 @@ export const NotificationSchema = Joi.object().keys({
   userId: Joi.number().required(),
   organizationId: Joi.number().required(),
   messageType: Joi.string().required(),
-  message: Joi.string().required()
+  message: Joi.string().required(),
+  read: Joi.boolean().required()
 });
