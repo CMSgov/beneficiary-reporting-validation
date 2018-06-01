fec640bd501b516c110dc47c03e152d41b0d5324
PetePlays <peteplays@hotmail.com>
2018-06-01 14:03:55 -0400

    removed `.max` from notification schema organizationId

--- lib/schema/notification.ts
+++ lib/schema/notification.ts
@@ -7,7 +7,7 @@ export enum NotificationMessageTypes {
 
 export const NotificationSchema = Joi.object().keys({
   userId: Joi.number().required(),
-  organizationId: Joi.number().max(11).required(),
+  organizationId: Joi.number().required(),
   messageType: Joi.string().valid(Object.values(NotificationMessageTypes)).required(),
   message: Joi.string().max(255).required(),
   read: Joi.boolean().required()
