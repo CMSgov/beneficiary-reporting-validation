4bd8cd182588ca8c3663ce2d22cf14b504c4e3ae
PetePlays <peteplays@hotmail.com>
2018-06-08 12:19:53 -0400

    added option to allow value to be an empty string or null

--- lib/schema/submission.ts
+++ lib/schema/submission.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export const SubmissionSchema = Joi.object().keys({
   attribute: Joi.string().max(255).required(),
-  value: Joi.string().max(255).required(),
+  value: Joi.string().max(255).allow(null).empty(''),
   scope: Joi.string().max(255).required(),
   comments: Joi.string().allow(null),
 });
