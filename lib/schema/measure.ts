b086b77b6b09d371bb62a1f1c21e955284aa8222
James Garrett <jamescgarrett@gmail.com>
2018-08-08 13:04:25 -0400

    Removing status field and bumping version

--- lib/schema/measure.ts
+++ lib/schema/measure.ts
@@ -20,7 +20,6 @@ export enum MeasureConfirmedOptions {
 
 export const MeasureMap = {
   name: Joi.string().max(128).regex(Regexes.lettersNumbersAndSymbolsOnly).required(), // candidate key, not updatable
-  status: Joi.string().valid(Object.values(MeasureStatus)).optional().allow(null), // TODO: this is not updatable and thus shouldn't be sent...
   confirmed: Joi.string().valid(Object.values(MeasureConfirmedOptions)).optional().allow(null),
   skippedReason: Joi.string().max(1024).optional().allow(null),
   helpDeskTicket: Joi.string().max(15).optional().allow(null),
