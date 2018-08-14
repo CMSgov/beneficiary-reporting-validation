1ea812790a2da7f01e5c01591c9008fdbca01ac3
JR McEntee <john.mcentee@semanticbits.com>
2018-08-14 11:35:37 -0400

    Cleanup per James

--- lib/schema/beneficiary.ts
+++ lib/schema/beneficiary.ts
@@ -7,44 +7,29 @@ export enum Gender {
   Unknown = 'UNKNOWN',
 }
 
-
-// These fields are potentially required based on the bene operation being performed.
-const firstName = Joi.string().max(128).regex(Regexes.lettersAndSymbolsOnly);
-const lastName = Joi.string().max(128).regex(Regexes.lettersAndSymbolsOnly);
-const gender = Joi.string().valid(Object.values(Gender));
-const dateOfBirth = Joi.date();
+const fields = {
+  firstName: Joi.string().max(128).regex(Regexes.lettersAndSymbolsOnly),
+  lastName: Joi.string().max(128).regex(Regexes.lettersAndSymbolsOnly),
+  gender: Joi.string().valid(Object.values(Gender)),
+  dateOfBirth: Joi.date(),
+  mrn: Joi.string().max(128).allow(null),
+  comments: Joi.string().allow(null),
+  skippedReason: Joi.string().allow(null),
+  medicalRecordFound: Joi.string().allow(null),
+  medicalNotQualifiedReason: Joi.string().allow(null),
+  medicalNotQualifiedDate: Joi.date().allow(null),
+  clinicId: Joi.number().allow(null),
+  qualificationComments: Joi.string().allow(null)
+};
 
 // This schema includes required fields, should be used when creating a bene.
 export const BeneficiaryMap = {
-  firstName: firstName.required(),
-  lastName: lastName.required(),
-  gender: gender.required(),
-  dateOfBirth: dateOfBirth.required(),
-  mrn: Joi.string().max(128).allow(null),
-  comments: Joi.string().allow(null),
-  skippedReason: Joi.string().allow(null),
-  medicalRecordFound: Joi.string().allow(null),
-  medicalNotQualifiedReason: Joi.string().allow(null),
-  medicalNotQualifiedDate: Joi.date().allow(null),
-  clinicId: Joi.number().allow(null),
-  qualificationComments: Joi.string().allow(null)
-};
-
-// This schema only includes optional fields. Should be used on updates.
-export const BeneficiaryFields = {
-  firstName: firstName,
-  lastName: lastName,
-  gender: gender,
-  dateOfBirth: dateOfBirth,
-  mrn: Joi.string().max(128).allow(null),
-  comments: Joi.string().allow(null),
-  skippedReason: Joi.string().allow(null),
-  medicalRecordFound: Joi.string().allow(null),
-  medicalNotQualifiedReason: Joi.string().allow(null),
-  medicalNotQualifiedDate: Joi.date().allow(null),
-  clinicId: Joi.number().allow(null),
-  qualificationComments: Joi.string().allow(null)
+  ...fields,
+  firstName: fields.firstName.required(),
+  lastName: fields.lastName.required(),
+  gender: fields.gender.required(),
+  dateOfBirth: fields.dateOfBirth.required()
 };
 
 export const BeneficiarySchema = Joi.object().keys(BeneficiaryMap);
-export const BeneficiaryFieldsSchema = Joi.object().keys(BeneficiaryFields);
+export const BeneficiaryFieldsSchema = Joi.object().keys(fields);
