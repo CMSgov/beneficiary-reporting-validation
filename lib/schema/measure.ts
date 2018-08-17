e6135e5b7c9896dae322d4569e61a7237b635503
jpattersonz <jeffrey.patterson@semanticbits.com>
2018-08-17 08:57:22 -0400

    removed the obsolete confirmed property and the readonly skippedReason property from the Measure schema

--- lib/schema/measure.ts
+++ lib/schema/measure.ts
@@ -1,20 +1,8 @@
 import * as Joi from 'joi';
 import { Regexes } from '../regexes';
 
-export enum MeasureConfirmedOptions {
-  Y = 'Y',
-  N = 'N',
-  NCD = 'NCD',
-  DE = 'DE',
-  NOCAR = 'NOCAR',
-  NOAGE = 'NOAGE',
-  NOGENDER = 'NOGENDER'
-}
-
 export const MeasureMap = {
   name: Joi.string().max(128).regex(Regexes.lettersNumbersAndSymbolsOnly).required(), // candidate key, not updatable
-  confirmed: Joi.string().valid(Object.values(MeasureConfirmedOptions)).optional().allow(null),
-  skippedReason: Joi.string().max(1024).optional().allow(null),
   helpDeskTicket: Joi.string().max(15).optional().allow(null),
   comments: Joi.string().optional().allow(null)
 };
