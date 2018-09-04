1bb5d079443c29613660778ac1efc545c6d520c9
James Garrett <jamescgarrett@gmail.com>
2018-09-04 08:57:41 -0400

    Updating comments fields to have max character limit

--- lib/schema/beneficiary.ts
+++ lib/schema/beneficiary.ts
@@ -13,13 +13,13 @@ export const BeneficiaryMap = {
   gender: Joi.string().valid(Object.values(Gender)),
   dateOfBirth: Joi.date(),
   mrn: Joi.string().max(128).allow(null),
-  comments: Joi.string().allow(null),
+  comments: Joi.string().max(1000).allow(null),
   skippedReason: Joi.string().allow(null),
   medicalRecordFound: Joi.string().allow(null),
   medicalNotQualifiedReason: Joi.string().allow(null),
   medicalNotQualifiedDate: Joi.date().allow(null),
   clinicId: Joi.number().allow(null),
-  qualificationComments: Joi.string().allow(null)
+  qualificationComments: Joi.string().max(1000).allow(null)
 };
 
 export const BeneficiarySchema = Joi.object().keys(BeneficiaryMap);
--- lib/schema/measure.ts
+++ lib/schema/measure.ts
@@ -4,7 +4,7 @@ import { Regexes } from '../regexes';
 export const MeasureMap = {
   name: Joi.string().max(128).regex(Regexes.lettersNumbersAndSymbolsOnly).required(), // candidate key, not updatable
   helpDeskTicket: Joi.string().max(15).optional().allow(null),
-  comments: Joi.string().optional().allow(null)
+  comments: Joi.string().max(1000).optional().allow(null)
 };
 
 export const MeasureSchema = Joi.object(MeasureMap);
