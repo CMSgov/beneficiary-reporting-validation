419210ff00295981f3e5634397a6812bf1be15b7
James Garrett <jamescgarrett@gmail.com>
2018-09-18 13:09:52 -0400

    Updating bene complex schema

--- lib/schema/submission.ts
+++ lib/schema/submission.ts
@@ -3,8 +3,7 @@ import * as Joi from 'joi';
 export const SubmissionMap = {
   attribute: Joi.string().max(255).trim().required(), // composite candidate key
   value: Joi.string().max(255).allow(null).empty(''),
-  scope: Joi.string().max(255).allow(null).empty(''), // composite candidate key
-  comments: Joi.string().allow(null),
+  scope: Joi.string().max(255).allow(null).empty('')
 };
 
 export const SubmissionSchema = Joi.object(SubmissionMap);
---
+++ test/beneficiary-complex.spec.ts
@@ -0,0 +1,84 @@
+import * as Joi from 'joi';
+import { BeneficiaryComplexSchema } from '../lib/schema/beneficiary.complex';
+
+const goodMeasure = {
+    name: "PREV-13",
+    helpDeskTicket: null,
+    comments: "string",
+};
+
+const goodSubmission = {
+    attribute: "patient-was-screened",
+    value: "Y",
+    scope: "2016-10-20"
+};
+
+const goodBene = {
+    id: 183635540,
+    firstName: "John",
+    lastName: "Doe",
+    dateOfBirth: "1955-11-06T00:00:00.000Z",
+    comments: "Some comments here",
+    medicalRecordFound: "NO",
+    medicalNotQualifiedReason: "IN_HOSPICE",
+    medicalNotQualifiedDate: "2007-11-06T00:00:00.000Z",
+    clinicId: 2536773,
+    qualificationComments: "This is qualification comment",
+}
+
+describe('BeneficiaryComplexSchema', () => {
+  it('should validate correctly', () => {
+    const result = Joi.validate({
+        ...goodBene,
+        measures: [{
+            ...goodMeasure,
+            submissions: [{
+                ...goodSubmission
+            }]
+        }]
+    }, BeneficiaryComplexSchema);
+    expect(result.error).toBeNull();
+  });
+  it('should fail validation if there are extra bene properties', () => {
+    const result = Joi.validate({
+        ...goodBene,
+        bogus: 'property',
+        measures: [{
+            ...goodMeasure,
+            submissions: [{
+                ...goodSubmission
+            }]
+        }]
+    }, BeneficiaryComplexSchema);
+    console.log(result);
+    expect(result.error).toBeTruthy();
+  });
+  it('should fail validation if there are extra measure properties', () => {
+    const result = Joi.validate({
+        ...goodBene,
+        measures: [{
+            bogus: 'property',
+            ...goodMeasure,
+            submissions: [{
+                ...goodSubmission
+            }]
+        }]
+    }, BeneficiaryComplexSchema);
+    console.log(result);
+    expect(result.error).toBeTruthy();
+  });
+  it.only('should fail validation if there are extra submission properties', () => {
+    const result = Joi.validate({
+        ...goodBene,
+        measures: [{
+            ...goodMeasure,
+            submissions: [{
+                bogus: 'property',
+                ...goodSubmission
+            }]
+        }]
+    }, BeneficiaryComplexSchema);
+    console.log(result);
+    expect(result.error).toBeTruthy();
+  });
+});
