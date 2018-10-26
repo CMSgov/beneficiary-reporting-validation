8e7643eef90f076d0c122815000f51d4bea4f809
Chetan Munegowda <chetan.munegowda@semanticbits.com>
2018-10-26 15:49:02 -0400

    QPPBSR-5174: fix randomatic lib version issues

--- lib/schema/clinic.ts
+++ lib/schema/clinic.ts
@@ -8,7 +8,8 @@ export const ClinicMap = {
   address2: Joi.string().max(100).regex(Regexes.validAddress, 'ValidAddress').optional().allow(null),
   city: Joi.string().max(50).regex(Regexes.lettersAndSymbolsOnly, 'LettersAndSymbolsOnly').optional().allow(null),
   state: Joi.string().length(2).regex(Regexes.stateAbbreviations).optional().allow(null),
-  zipCode: Joi.string().min(5).max(10).regex(Regexes.validZipCode, 'ValidZipCode').optional().allow(null)
+  zipCode: Joi.string().min(5).max(10).regex(Regexes.validZipCode, 'ValidZipCode').optional().allow(null),
+  organizationId: Joi.number().required()
 };
 
 export const ClinicSchema = Joi.object(ClinicMap);
