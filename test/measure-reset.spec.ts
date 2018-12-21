3b13b54bd17a11902ed9e0f627f73bffa011f207
Darrin Bennett <darrinbennett@Darrins-MBP.attlocal.net>
2018-12-21 09:24:23 -0500

    update schema for measure reset endpoint

--- test/measure-reset.spec.ts
+++ test/measure-reset.spec.ts
@@ -3,25 +3,25 @@ import { MeasureResetSchema } from '../lib/schema/measure-reset';
 
 describe('MeasureResetSchema', () => {
   it('should validate correctly', () => {
-    const result = Joi.validate([
-      'CARE-1',
-      'CARE-2',
-      'PREV-10'
-    ], MeasureResetSchema);
+    const result = Joi.validate({
+      measureSelections:['CARE-1', 'CARE-2', 'PREV-10'],
+      clearBeneficiaryConfirmation: false
+    }, MeasureResetSchema);
     expect(result.error).toBeNull();
   });
 
   it('should not allow non string values in array', () => {
-    const result = Joi.validate([
-      1, 2, 'CARE-1'
-    ], MeasureResetSchema);
+    const result = Joi.validate({
+      measureSelections: [1, 2, 'CARE-1'],
+      clearBeneficiaryConfirmation: false
+    }, MeasureResetSchema);
     expect(result.error).not.toBeNull();
   });
 
-  it('should not allow non array type', () => {
-    const result = Joi.validate({
-      bad: 1
-    }, MeasureResetSchema);
+  it('should not allow non object type', () => {
+    const result = Joi.validate([
+      { bad: 1 }
+    ], MeasureResetSchema);
     expect(result.error).not.toBeNull();
   });
 });
