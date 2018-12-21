9db0608c938905c3ff9b667013a4c96328995170
Darrin Bennett <darrinbennett@Darrins-MBP.attlocal.net>
2018-12-21 12:56:31 -0500

    change names of properties in reset measures validation

--- test/measure-reset.spec.ts
+++ test/measure-reset.spec.ts
@@ -4,16 +4,16 @@ import { MeasureResetSchema } from '../lib/schema/measure-reset';
 describe('MeasureResetSchema', () => {
   it('should validate correctly', () => {
     const result = Joi.validate({
-      measureSelections:['CARE-1', 'CARE-2', 'PREV-10'],
-      clearBeneficiaryConfirmation: false
+      measures:['CARE-1', 'CARE-2', 'PREV-10'],
+      confirmation: false
     }, MeasureResetSchema);
     expect(result.error).toBeNull();
   });
 
   it('should not allow non string values in array', () => {
     const result = Joi.validate({
-      measureSelections: [1, 2, 'CARE-1'],
-      clearBeneficiaryConfirmation: false
+      measures: [1, 2, 'CARE-1'],
+      confirmation: false
     }, MeasureResetSchema);
     expect(result.error).not.toBeNull();
   });
